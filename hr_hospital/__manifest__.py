cbb9151311cfe7b8624612a95328c0ae291e063c
Danylo <danulo.senuk@gmail.com>
2025-03-16 16:27:15 +0100

    [IMP] hr_hospital: add manifest description

---
+++ hr_hospital/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Hospital',
+    'version': '17.0.1.0.0',
+    'author': "Odoo School",
+    'website': 'odoo.school',
+    'license': 'OPL-1',
+    'category': 'Human Resources',
+    'depends':[
+        'base',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'maintainer': "Danylo Senyuk",
+    'images': ['static/desscription/icon.png',],
+}

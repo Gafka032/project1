01bc7d18c4cd1c8aa91706149dcc3b906a3ae7f3
Danylo <danulo.senuk@gmail.com>
2025-03-21 10:54:41 +0100

    [REF] hr_hospital: delete comments

--- hr_hospital/models/hr_hospital_visit.py
+++ hr_hospital/models/hr_hospital_visit.py
@@ -1,13 +1,12 @@
 from odoo import models, fields
 
 
-# Оголошення класу HrHospitalVisit,
-# який представляє модель "Візити в лікарню"
+
 class HrHospitalVisit(models.Model):
-    # Унікальне внутрішнє ім'я моделі в Odoo
+
     _name = 'hr.hospital.visit'
-    # Короткий опис моделі, що використовується в інтерфейсі та документації
+
     _description = 'Visit'
 
-    # Поле типу Char, яке зберігає назву або опис візиту
+
     name = fields.Char()

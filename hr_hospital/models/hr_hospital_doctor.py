b77cd8685d184070f3f8b4f3f63429671336aeb6
Danylo <danulo.senuk@gmail.com>
2025-03-23 22:38:27 +0100

    [IMP] hr_hospital: add new fields to doctor model

--- hr_hospital/models/hr_hospital_doctor.py
+++ hr_hospital/models/hr_hospital_doctor.py
@@ -1,10 +1,51 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class HrHospitalDoctor(models.Model):
 
     _name = 'hr.hospital.doctor'
-
+    _inherit = 'hr.hospital.person.mixin'
     _description = 'Doctor'
 
-    name = fields.Char()
+    name = fields.Char(
+        string='Name, Surname',
+        help="Doctor's name",
+    )
+
+    active = fields.Boolean(
+        default=True,
+        groups='base.group_system',
+        copy=False,
+    )
+
+    description = fields.Text(
+        index=True,
+        translate=True,
+    )
+
+    res_partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Contact'
+    )
+
+    speciality_id = fields.Many2one(
+        comodel_name='hr.hospital.doctor.speciality',
+        string='Speciality',
+        help="Doctor's speciality"
+    )
+
+    is_intern = fields.Boolean(
+        help="Is doctor an intern?",
+        default=False,
+        copy=False,
+    )
+
+    mentor_id = fields.Many2one(
+        comodel_name='hr.hospital.doctor',
+        string='Mentor',
+        domain="[('is_intern', '=', True)]",
+        help="Intern's mentor",
+    )
---
+++ hr_hospital/models/res_partner.py
@@ -0,0 +1,14 @@
+import logging
+
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    hr_hospital_doctor_ids = fields.Many2many(
+        comodel_name='hr.hospital.doctor',
+        string='Hospital Doctors',
+    )

dbe68b42a861bd071d4cd9a36e608109af24c5b6
Danylo <danulo.senuk@gmail.com>
2025-03-24 15:33:27 +0100

    [IMP] hr_hospital: add diagnosis model

---
+++ hr_hospital/models/hr_hospital_diagnosis.py
@@ -0,0 +1,62 @@
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+class HrHospitalDiagnosis(models.Model):
+    _name = 'hr.hospital.diagnosis'
+    _description = 'Diagnosis'
+
+    name = fields.Char()
+
+    active = fields.Boolean(
+        default=True,
+        groups='base.group_system',
+        copy=False,
+    )
+
+    patient_visit_id = fields.Many2one(
+        related='patient_id.visit_id',
+        store=True,
+        string='Visit',
+        help='Visit',
+    )
+
+    patient_id = fields.Many2one(
+        comodel_name='hr.hospital.patient',
+        string='Patient',
+        help='Patient',
+    )
+
+    doctor_id = fields.Many2one(
+        comodel_name='hr.hospital.doctor',
+        string='Doctor',
+        help='Doctor',
+    )
+
+    is_intern = fields.Boolean(
+        related='doctor_id.is_intern',
+        store=True,
+    )
+
+    disease_id = fields.Many2one(
+        comodel_name='hr.hospital.disease',
+        string='Disease',
+    )
+
+    appointment_treatment = fields.Text(
+        string='Appointment treatment',
+        help='Write appointment treatment',
+    )
+
+    is_approved = fields.Boolean(
+        string='Approved by mentor doctor',
+    )
+
+    @api.model
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        if not res.is_intern:
+            res.update({'is_approved': True})
+        return res

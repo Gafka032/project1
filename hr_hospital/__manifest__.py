d73bca1e409a3e23d341fc4cb5f423aa88e8f750
danylo <danulo.senuk@gmail.com>
2025-04-06 15:14:01 +0300

    [IMP] hr_hospital: change menu sequences

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -10,16 +10,19 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+
         'wizard/hr_hospital_personal_doctor_wizard_views.xml',
         'wizard/hr_hospital_report_diseases_wizard_views.xml',
+
         'views/hr_hospital_visit_views.xml',
+        'views/hr_hospital_diagnosis_views.xml',
         'views/hr_hospital_menu.xml',
         'views/hr_hospital_doctor_views.xml',
         'views/hr_hospital_doctor_speciality_views.xml',
         'views/hr_hospital_patient_views.xml',
         'views/hr_hospital_disease_views.xml',
-        'views/hr_hospital_diagnosis_views.xml',
         'views/hr_hospital_disease_type_views.xml',
+
         'data/hr_hospital_disease_data.xml',
     ],
     'demo': [
